3a0d922ca362e94f4e8d42b7372fb7e49effdccb
Niyakit <niyakit123@gmail.com>
2023-04-05 18:22:19 +0700

    update update.sh 1822

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 sudo docker compose down
-sudo docker compose up -d --build --remove-orphans
+sudo docker compose up # -d --build --remove-orphans
 pip install -r requirements.txt
 echo "Waiting for the database to be ready..."
 sleep 10
