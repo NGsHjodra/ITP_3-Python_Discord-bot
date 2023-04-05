2aced7e3241a30f481f5d91134be6027d3644288
Niyakit <niyakit123@gmail.com>
2023-04-06 02:42:09 +0700

    modified update.sh no daemon

--- update.sh
+++ update.sh
@@ -5,5 +5,5 @@ pip install -r requirements.txt
 echo "Waiting for the database to be ready..."
 sleep 10
 sudo docker ps
-# python3 main.py
-/home/ubuntu/.local/bin/gunicorn -b 0.0.0.0:5000 main:app --preload --daemon
+python3 main.py
+# /home/ubuntu/.local/bin/gunicorn -b 0.0.0.0:5000 main:app --preload --daemon
