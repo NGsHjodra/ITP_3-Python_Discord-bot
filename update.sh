b3e85224cb79fc79ac470c12f43f5529ce915e29
Niyakit <niyakit123@gmail.com>
2023-04-05 15:39:03 +0700

    update docker-compose.yml 1538

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
-sudo docker-compose down
-sudo docker-compose up -d --build
+sudo docker compose down
+sudo docker compose up -d # --build
 pip install -r requirements.txt
 echo "Waiting for the database to be ready..."
 sleep 10
