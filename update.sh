f7c2f9c275f51b70f2045c9b038b49057e9bbc4b
Niyakit <niyakit123@gmail.com>
2023-04-05 15:04:01 +0700

    update update.sh 1503

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 sudo docker compose down
-sudo docker compose up -d --build
+sudo docker compose up -d # --build
 echo "Waiting for the database to be ready..."
 sleep 5
 pip install -r requirements.txt
