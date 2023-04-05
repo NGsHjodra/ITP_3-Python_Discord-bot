ba11c063e93b9e78c54099ab9c154c4ff45bcdc7
Niyakit <niyakit123@gmail.com>
2023-04-06 02:02:38 +0700

    add gunicorn

--- update.sh
+++ update.sh
@@ -1,7 +1,9 @@
+sudo pkill gunicorn
 sudo docker compose down
 sudo docker compose up -d --build --remove-orphans
 pip install -r requirements.txt
 echo "Waiting for the database to be ready..."
 sleep 10
 sudo docker ps
-python3 main.py
+# python3 main.py
+/home/ubuntu/.local/bin/gunicorn -b 0.0.0.0:5000 main:app --daemon
