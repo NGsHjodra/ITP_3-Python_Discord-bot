a045ab38df6a6b2ff30aa7acea2080c7e868b363
Niyakit <niyakit123@gmail.com>
2023-04-05 15:09:19 +0700

    update update.sh 1508

--- update.sh
+++ update.sh
@@ -2,5 +2,7 @@ sudo docker compose down
 sudo docker compose up -d # --build
 echo "Waiting for the database to be ready..."
 sleep 5
+sudo service postgresql status
+sleep 1
 pip install -r requirements.txt
 python3 main.py
